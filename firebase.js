8c23b9173c53064f834979d5a3781c13ae7f5b01
Awsmin <103874344+HowDoYouWriteACommentLineAgain@users.noreply.github.com>
2025-11-09 20:53:09 +0800

    moved stuff from index.html to its own html file plus prepping file for edit tests ;

--- firebase.js
+++ firebase.js
@@ -3,5 +3,5 @@ import { getFirestore } from 'firebase/firestore';
 import config from './config.js';
 
 const app = initializeApp(config.firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
 export default db;
